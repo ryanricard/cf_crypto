4286a861984e1f74fd4b6069bb34379e4e22a802
Ryan Ricard <me@ryanricard.com>
2012-06-15 14:02:55 -0500

    Fixed tagging in DES spec.

--- spec/cf_crypto_spec.rb
+++ spec/cf_crypto_spec.rb
@@ -4,17 +4,17 @@ describe "CfCrypto" do
 
 	let(:sensitive_information){ "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }
 
-  describe "DES" do
+	describe "DES" do
 
 		let(:des_key){ "sAd!DF%lX^ka&Gs*jHd" }
 
-    it "should encrypt and decrypt a string" do
-    	result = CfCrypto::DES.encrypt(sensitive_information, des_key)
-    	result.should_not eq(sensitive_information)
-    	result = CfCrypto::DES.decrypt(result, des_key)
+		it "should encrypt and decrypt a string" do
+			result = CfCrypto::DES.encrypt(sensitive_information, des_key)
+			result.should_not eq(sensitive_information)
+			result = CfCrypto::DES.decrypt(result, des_key)
 			result.should eq(sensitive_information)
-   	end
+		end
 
-  end
+	end
 
 end
